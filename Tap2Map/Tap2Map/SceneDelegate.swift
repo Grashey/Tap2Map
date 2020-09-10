64c85fb858b9821375cc4baa61025ea5d04c4e63
Aleksandr Fetisov <grashey@mail.ru>
2020-09-10 14:46:58 +0300

    ShieldView for secure inactive app

--- Tap2Map/Tap2Map/SceneDelegate.swift
+++ Tap2Map/Tap2Map/SceneDelegate.swift
@@ -13,6 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     var coordinator: ApplicationCoordinator?
+    var shieldView: UIImageView?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -32,13 +33,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        shieldView?.removeFromSuperview()
+        shieldView = nil
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
+        shieldView = UIImageView(frame: self.window!.frame)
+        shieldView?.image = UIImage(named: "mickey mouse")
+        shieldView?.contentMode = .scaleAspectFit
+        self.window?.addSubview(shieldView!)
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
